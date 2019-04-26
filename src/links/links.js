9d2b807af8d437c283fccbc96b9ea425540574a7
Santosh <g.santosh.sunny@gmail.com>
2019-04-26 18:51:07 +0530

    Initial commit

---
+++ src/links/links.js
@@ -0,0 +1,21 @@
+// import "../assets/css/bootstrap.min.css";
+import "../assets/css/hipster_cards.css";
+import "../presentation-assets/css/demo.css";
+
+// // Fonts and icons
+import "../assets/css/pe-icon-7-stroke.css";
+import "../assets/font-awesome/font-awesome.min.css";
+import "../assets/font-awesome/googleapis.css";
+// import "https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css";
+// import "https://fonts.googleapis.com/css?family=Playfair+Display|Raleway:700,100,400|Roboto:400,700|Playfair+Display+SC:400,700";
+
+// javascript
+// import "bootstrap";
+// import "bootstrap"
+// import "../assets/js/hipster-cards.js";
+
+// images
+// import lifestyle from "../assets/img/lifestyle-9.jpg";
+// import home_1 from "../assets/img/home-1.jpg";
+
+// console.log(home_1);
